5e94ce2d3935636e37c0e9d40f589c7ca60816b4
Parth Kulkarni <parthkulkarnimusic@gmail.com>
2025-02-15 17:36:06 +0530

    Added Dynamic Data

--- flaskapp.py
+++ flaskapp.py
@@ -1,8 +1,40 @@
-from flask import Flask , render_template
+from flask import Flask, jsonify , render_template
 app = Flask(__name__) #the name variable will tell from where it was invoked
+
+TRANSACTIONS = [
+    {
+        'id':1,
+        'amount':20000,
+        'desc': 'Goa Trip',
+        'date': '27-01-2025'
+    },
+    {
+        'id':2,
+        'amount':4000,
+        'desc': 'Fan',
+        'date': '3-02-2025'
+    },
+    {
+        'id':3,
+        'amount':30,
+        'desc': 'Ice cream',
+        'date': '15-02-2025'
+    },
+    {
+        'id':4,
+        'amount':40,
+        'desc': 'Notebook',
+        'date': '15-02-2025'
+    }
+]
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",trans=TRANSACTIONS)
+
+@app.route("/api/transactions")
+def list_transactions():
+    return jsonify(TRANSACTIONS)
+
 if __name__ == "__main__":
     app.run(debug=True)
