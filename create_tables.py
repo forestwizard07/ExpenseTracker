fad8c9705d2771d2eee034bd886d6cc8583e6fc7
Parth Kulkarni <parthkulkarnimusic@gmail.com>
2025-05-14 01:29:58 +0530

    minor date format fixes

--- create_tables.py
+++ create_tables.py
@@ -10,7 +10,7 @@ load_dotenv()
 # Replace these with your actual database connection details
 conn = psycopg2.connect(os.environ.get('DATABASE_URL'))
 cursor = conn.cursor()  # Use RealDictCursor to fetch data as a dictionary
-cursor.execute("ALTER TABLE expenselog ADD category varchar(20);")
+cursor.execute("ALTER TABLE expenselog ALTER COLUMN dateofLog TYPE DATE USING dateofLog::DATE;")
 #rows = cursor.fetchall()
 #print(rows)  # Print the first 10 rows to the console
 conn.commit()
